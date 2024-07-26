6a1853e93be96d056eff775ef44466f4a9a4feaa
Luiz Araujo <87837854+ludolua@users.noreply.github.com>
2024-07-26 10:31:35 -0300

    Delete PQF directory

--- PQF/static/a.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard() {
-    var formattedQuery = document.querySelector('pre code');
-    var range = document.createRange();
-    range.selectNode(formattedQuery);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    alert('Copied to clipboard!');
-}
